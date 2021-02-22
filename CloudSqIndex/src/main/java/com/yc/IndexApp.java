5193e633aa8a6380006094e69629995444352a99
hahas <hahas@LAPTOP-TB2DCIL3>
2021-02-22 14:31:12 +0800

    .

---
+++ CloudSqIndex/src/main/java/com/yc/IndexApp.java
@@ -0,0 +1,16 @@
+package com.yc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+
+@SpringBootApplication
+@EnableEurekaClient
+public class IndexApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndexApp.class, args);
+	}
+}
